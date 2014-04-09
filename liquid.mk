381884816da4d4f2e83db3e08106dccd7e5f911d
CPA-Poke <brock.shahan@cox.net>
2014-04-08 20:58:37 -0500

    Liquify!

--- liquid.mk
+++ liquid.mk
@@ -4,5 +4,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
        BUILD_ID=KOT49H BUILD_FINGERPRINT="htc/htc_europe/htc_m8:4.4.2/KOT49H/325784.5:user/release-keys" \
        PRIVATE_BUILD_DESC="1.54.401.5 CL325784 release-keys"
 
-PRODUCT_NAME := cm_m8
+PRODUCT_NAME := liquid_m8
 PRODUCT_DEVICE := m8
